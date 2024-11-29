572f5fb5b964e3c322422040da7eadbca38f137f
smohammedakhter <smohammedakhter@apple.com>
2024-11-29 09:13:27 +0530

    commit 3

--- src/OOP/BankAccount.java
+++ src/OOP/BankAccount.java
@@ -1,4 +1,49 @@
 package OOP;
 
 public class BankAccount {
+
+    private double balance;
+    private String ownerName;
+
+    BankAccount(){
+        ownerName = null;
+        this.balance = 0;
+    }
+    BankAccount(String ownerName, double balance) {
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+        }
+        balance = balance + amount;
+    }
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance = balance - amount;
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if (balance >= 0) {
+            this.balance = balance;
+        }
+
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+    public void setOwnerName(String ownerName) {
+        if (ownerName != null) {
+            this.ownerName = ownerName;
+        }
+    }
 }
